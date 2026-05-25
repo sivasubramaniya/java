c7d8b0645dafd5fdafae3ebf8a37007d34b97e81
sivasubramaniya <siva74bca@gmail.com>
2026-05-25 16:33:07 +0530

    first commit

---
+++ scanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class c{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        System.out.print("enter the number:"+a);
+        
+    }
+}
+    
+
---
+++ typecasting.java
@@ -0,0 +1,13 @@
+class typecasting {
+    public static void main(String args[])
+    {
+       int a=10;
+       double b=a;
+       System.out.println("value of b:"+b);
+       double c=9.99;
+       int d=(int) c;
+    System.out.print("value of d:"+d);
+    }
+
+    
+}
